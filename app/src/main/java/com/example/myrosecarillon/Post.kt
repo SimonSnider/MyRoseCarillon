a3b3c1b03443820419d697dcac8d22c4fc343f39
snidersa <snidersa@rose-hulman.edu>
2020-07-20 14:09:10 -0400

    added database objects

---
+++ app/src/main/java/com/example/myrosecarillon/Post.kt
@@ -0,0 +1,6 @@
+package com.example.myrosecarillon
+
+import java.util.*
+
+class Post(postID: Int, date: Date, likes: Int, downvotes: Int, user: User, song: Song) {
+}
---
+++ app/src/main/java/com/example/myrosecarillon/Song.kt
@@ -0,0 +1,4 @@
+package com.example.myrosecarillon
+
+class Song (SongID: Int, midi: String, title: String, score: Score, creator: User) {
+}
