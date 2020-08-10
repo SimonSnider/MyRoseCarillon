1120aea21aae384c99751ddcd0a4fae4724eb043
snidersa <snidersa@rose-hulman.edu>
2020-08-10 16:54:07 -0400

    my songs page implemented

--- app/src/main/java/com/example/myrosecarillon/MySongsAdapter.kt
+++ app/src/main/java/com/example/myrosecarillon/MySongsAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.myrosecarillon
 
+import android.app.AlertDialog
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
@@ -56,13 +57,25 @@ class MySongsAdapter(val context: Context):  RecyclerView.Adapter<SongViewHolder
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.vote_song_card, parent, false)
-        return SongViewHolder(view)
+        val view = LayoutInflater.from(context).inflate(R.layout.my_song_card, parent, false)
+        return SongViewHolder(view, this)
     }
 
     override fun getItemCount() = songs.size
 
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
         holder.bind(songs[position])
+    }
+
+    fun showDeleteDialog(position: Int){
+        val builder = AlertDialog.Builder(context, R.style.AlertDialogTheme)
+        builder.setTitle("Are you sure you want to delete the song ${songs[position].title}?")
+        builder.setPositiveButton(android.R.string.ok) {_, _ ->
+            Log.d(Constants.TAG, "Deleting song ${songs[position].id}")
+        }
+        builder.setNegativeButton(android.R.string.cancel) {_,_ ->
+
+        }
+        builder.create().show()
     }
 }
--- app/src/main/java/com/example/myrosecarillon/SongViewHolder.kt
+++ app/src/main/java/com/example/myrosecarillon/SongViewHolder.kt
@@ -1,16 +1,30 @@
 package com.example.myrosecarillon
 
+import android.app.AlertDialog
+import android.provider.SyncStateContract
+import android.util.Log
 import android.view.View
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myrosecarillon.constants.Constants
 import com.example.myrosecarillon.objects.Song
 import kotlinx.android.synthetic.main.my_song_card.view.*
 import kotlinx.android.synthetic.main.vote_song_card.view.card_title
 
-class SongViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+class SongViewHolder(itemView: View, val adapter: MySongsAdapter) : RecyclerView.ViewHolder(itemView){
     private var cardView: CardView = itemView.my_song_card
 
     fun bind(song: Song){
         cardView.card_title.text = song.title
+        cardView.edit_song.setOnClickListener {
+            //TODO: Navigate to compose fragment with this song's midi
+            Log.d(Constants.TAG, "Editing song ${song.id}")
+        }
+        cardView.play_song.setOnClickListener {
+            Log.d(Constants.TAG, "Playing song ${song.id}")
+        }
+        cardView.delete_song.setOnClickListener{
+            adapter.showDeleteDialog(adapterPosition)
+        }
     }
 }
